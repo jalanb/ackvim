0c053d856fa0df3b0598fd666f9d7d27d8013839
J Alan Brogan <github@al-got-rhythm.net>
2015-10-09 03:59:25 +0100

    Take over the ack command with a function

--- ack_vack.py
+++ ack_vack.py
@@ -1,10 +1,12 @@
 """Script to change -v for an ack command to vack"""
 
+import os
 import re
 import sys
 
 def main(args):
-    words = ['ack']
+    ack = os.environ.get('ACK') or 'ack'
+    words = [ack]
     for word in args:
         if re.match('-[a-uw-z]*[vV][a-uw-z]*', word):
             words[0] = 'vack'
