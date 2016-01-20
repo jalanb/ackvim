177d6b8ed8f8448674e5f840f18df424709978a5
J Alan Brogan <github@al-got-rhythm.net>
2016-01-20 14:51:48 +0000

    Better search for the original ack (a perl script)

--- ack_vack.py
+++ ack_vack.py
@@ -3,10 +3,24 @@
 import os
 import re
 import sys
+import commands
+
+def which_ack():
+    ack = os.environ.get('ACK') or 'ack'
+    if not ack or not os.path.isfile(ack):
+        status, output = commands.getstatusoutput('which ack')
+        if status != os.EX_OK:
+            raise NotImplementedError('"which ack" failed: "%s"' % output)
+        ack = output
+    if not os.path.isfile(ack):
+        raise NotImplementedError('"%s" is not a file' % ack)
+    return ack
 
 def main(args):
-    ack = os.environ.get('ACK') or 'ack'
-    words = [ack]
+    if '-U' in sys.argv:
+        import pudb
+        pudb.set_trace()
+    words = [which_ack()]
     for word in args:
         if re.match('-[a-uw-z]*[vV][a-uw-z]*', word):
             words[0] = 'vack'
